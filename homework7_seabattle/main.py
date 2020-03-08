0126aab3c43b05365b340bf75d04c0e6f5109d56
tsypyshev.pi <ptsypyshev@gmail.com>
2020-03-08 18:18:07 +0500

    Added ships setup method, field draw method.

--- homework7_seabattle/main.py
+++ homework7_seabattle/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from models import Players, Field
+
 """
 Main file. Contains program execution logic.
 """
@@ -8,11 +10,36 @@ Main file. Contains program execution logic.
 __author__ = 'ptsypyshev'
 
 
+def create_field():
+    while True:
+        user_input = input('Введите размеры игрового поля через пробел или нажмите Enter для поля 10x10: ')
+        if not user_input:
+            return Field()
+        else:
+            try:
+                x, y = (int(i) for i in user_input.split())
+                if x > 20 or y > 20:
+                    print('Слишком большой размер поля. Попробуйте ещё раз')
+                    continue
+                break
+            except ValueError:
+                print('Введите целые числа для длины и ширины игрового поля, например 10 10')
+
+    return Field(x, y)
+
+
 def main():
     """
     Main method of seabattle game.
     """
-    pass
+    sb_field = create_field()
+    # sb_field.draw_field()
+    player1 = Players(input('Введите своё имя: '), 1)
+    player2 = Players('Python', 2)
+    print(player1, player2)
+    sb_field.draw()
+    player1_ships = sb_field.setup()
+    print(player1_ships)
 
 
 if __name__ == '__main__':
--- homework7_seabattle/models.py
+++ homework7_seabattle/models.py
@@ -2,6 +2,8 @@
 
 __author__ = 'ptsypyshev'
 
+import os
+
 
 class Players:
     """
@@ -12,22 +14,111 @@ class Players:
         self.name = name
         self.queue = queue
 
+    def __str__(self):
+        return f'Player name is {self.name}, queue is {self.queue}'
+
 
 class Field:
     """
     Defines Field object and its properties.
     """
-    default_size = (10, 10)
+    default_x = 10
+    default_y = 10
     default_ships = {
-        '4x': 1,
-        '3x': 2,
-        '2x': 3,
-        '1x': 4,
+        4: 1,
+        3: 2,
+        2: 3,
+        1: 4,
     }
 
-    def __init__(self, size=default_size, ships=default_ships):
-        self.size = size
+    def __init__(self, size_x=default_x, size_y=default_y, ships=default_ships):
+        self.size_x = size_x
+        self.size_y = size_y
         self.ships = ships
+        alphas = [chr(i) for i in range(1040, 1070) if i != 1049]
+        alphas = alphas[:self.size_x]
+        alphas.insert(0, '  ')
+        digits = tuple(i for i in range(1, self.size_y + 1))
+        self.seafield = [[' ' for j in range(size_y)] for i in range(size_x)]
+        self.seafield.insert(0, alphas)
+        for i in range(0, size_y + 1):
+            if 0 < i < 10:
+                self.seafield[i].insert(0, ' ' + str(i))
+            if 10 <= i:
+                self.seafield[i].insert(0, i)
+            self.seafield[i].append('          ')
+            self.seafield[i].extend(self.seafield[i])
+
+    def draw(self):
+        os.system('cls')
+        for i in range(self.size_x + 1):
+            for j in range((self.size_y + 1) * 2 + 1):
+                print(self.seafield[i][j], end=' | ')
+            print()
+
+    def check_neighbours(self, x, y):
+        try:
+            for i in range(y - 1, y + 1):
+                for j in range(x - 1, x + 1):
+                    if self.seafield[i][j] == '█' or self.seafield[i][j] == '*':
+                        return False
+        except IndexError:
+            print(f'Корабль выходит за пределы поля сражения {self.size_x}x{self.size_y}')
+            return False
+        return True
+
+    def get_position(self, ship):
+        inp = input(f'Введите через пробел координаты начала для {ship}-x палубного корабля, например Д7: ')
+        x_str, *y_str = inp
+        x = ''.join(self.seafield[0]).find(x_str) - 1
+        try:
+            y = int(''.join(y_str))
+            if x == -1 or x == 0 or y == 0:
+                raise ValueError
+        except ValueError:
+            print('Введите корректные координаты.')
+            return False
+        return x, y
+
+    def setup(self):
+        for ship_type, count in self.ships.items():
+            player_ships = []
+            for i in range(count):
+                while True:
+                    position = self.get_position(ship_type)
+                    if not position:
+                        continue
+                    x, y = position
+                    if 0 < x < self.size_x + 1 or 0 < self.size_y + 1 < 11:
+                        direction = input('Расположение горизонтальное? [Y/N] ')
+                        if direction.lower() == '' or direction.lower() == 'y' or direction.lower() == 'yes' or \
+                                direction.lower() == 'да':
+                            for j in range(ship_type):
+                                if not self.check_neighbours(x + j, y):
+                                    print(f'Невозможно установить корабль по этим координатам.')
+                                    break
+                            else:
+                                for j in range(ship_type):
+                                    self.seafield[y][x + j] = '█'
+                                player_ships.append(Ships(ship_type, (x, y), False))
+                                self.draw()
+                                break
+                        else:
+                            for j in range(ship_type):
+                                if not self.check_neighbours(x, y + j):
+                                    print(f'Невозможно установить корабль по этим координатам.')
+                                    break
+                            else:
+                                for j in range(ship_type):
+                                    self.seafield[y + j][x] = '█'
+                                player_ships.append(Ships(ship_type, (x, y), False))
+                                self.draw()
+                                break
+                    else:
+                        print(f'Введенные координаты находятся за пределами поля сражения {self.size_x}x{self.size_y}. Попробуйте ещё раз')
+        return player_ships
+    # def setup_comp(self):
+
 
 
 class Ships:
@@ -36,9 +127,10 @@ class Ships:
     """
     default_position = (0, 0)
 
-    def __init__(self, length=1, position=default_position, sunken=False):
+    def __init__(self, length=1, position=default_position, hor_direction=False, sunken=False):
         self.length = length
         self.position = position
+        self.hor_direction = hor_direction
         self.sunken = sunken
 
 
